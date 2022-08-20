d1cab1b192ce2405c4e342c1de4b1d45f4d4353b
Zain Ul Abideen <zainjaveed97@gmail.com>
2022-08-20 19:17:41 +0500

    working with js not with ts

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const tasks = require('./routes/tasks');
+const ConnectDatabase = require('./db/connect');
+require('dotenv').config();
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/v1/tasks', tasks);
+
+
+const port = process.env.LISTEN_PORT;
+
+// app.get('/', (req, res) => {
+//     res.end('Task Manager App!');
+// });
+
+
+async function RunServer() {
+    try {
+        await ConnectDatabase(process.env.MONGO_URI);
+
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    }
+    catch (error) {
+        console.log('Error Running Server: ', error);
+    }
+}
+
+RunServer();
